66093fb74e99a6f4d5854bece3078fc975ad168d
cyberblast <git@cyberblast.org>
2017-05-13 00:16:14 +0200

    nicer console demo output

--- ScreepsConsole/Program.cs
+++ ScreepsConsole/Program.cs
@@ -10,20 +10,27 @@ namespace ScreepsConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is only a demo (yet)");
+            Console.WriteLine("Press \"Ctrl + C\" anytime to quit");
+            Console.WriteLine();
             Console.Write("Please enter your screeps account email: ");
             string email = Console.ReadLine();
             Console.Write("Please enter your screeps account password: ");
             string password = Console.ReadLine();
-            Console.WriteLine("Press \"Ctrl + C\" anytime to quit");
+            Console.WriteLine();
 
             Client client = new Client(email, password);
-            //Console.WriteLine("Token: " + client.token);
             
             while (true)
             {
                 dynamic me = client.Me();
-                Console.WriteLine("User {0}, GCL: {1}, credits: {2}", me.username, me.gcl, me.credits);
-                System.Threading.Thread.Sleep(10000);
+                Console.Write("User {0}, GCL: {1} ", me.username, me.gcl);
+                for (int i = 0; i<6; i++)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    Console.Write(".");
+                }
+                Console.WriteLine();
             }         
         }
     }
